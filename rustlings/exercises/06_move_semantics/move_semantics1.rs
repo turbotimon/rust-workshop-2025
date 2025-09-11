91fca37e9f8e6b4fe4ba01742ae3ec564332393e
Timon Erhart <57718207+turbotimon@users.noreply.github.com>
2025-09-11 18:33:00 +0200

    finish 04

--- rustlings/exercises/06_move_semantics/move_semantics1.rs
+++ rustlings/exercises/06_move_semantics/move_semantics1.rs
@@ -1,14 +1,45 @@
 // TODO: Fix the compiler error in this function.
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec = vec;
 
     vec.push(88);
 
     vec
 }
 
+// ERTI first solution! Way to complicated!
+// fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+//     let vec_ = &mut vec;
+
+//     vec_.push(88);
+
+//     vec
+// }
+
 fn main() {
-    // You can optionally experiment here.
+    // // You can optionally experiment here.
+    // let vec0 = vec![22, 44, 66];
+    // let vec1 = fill_vec(vec0);
+
+    // let mut x = 7;
+    // let mut y = x;
+    // x = 8;
+    // println!("{x} {y}");
+
+    // let mut vec0 = vec0;
+    // vec0.push(42);
+
+    // let vec0 = vec0;
+    // println!("{vec0:?}");
+
+    let vec0 = vec![22, 44, 66];
+
+    let mut vec0 = vec0; // make mutable by shadowing
+    vec0.push(42);
+
+    let vec0 = vec0; // shadow again to make it immutable
+    println!("{vec0:?}"); // prints [22, 44, 66, 42]
+
 }
 
 #[cfg(test)]
